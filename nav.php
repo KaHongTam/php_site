c7d409e3c049aff4375eb7ebb6ce40ae3d532ae2
KaHongTam <kahongtam@hotmail.com>
2018-11-30 10:30:39 +0100

    nav.php aangemaakt en verwerkt op alle pagina

---
+++ nav.php
@@ -0,0 +1,11 @@
+<div class="nav" id="nav">
+        <a href="index.php" class="nav_menu">Home</a>
+        <a href="aanmelden.php" class="nav_menu">Hoe werkt het?</a>
+        <a href="shop.php?prijs=2" class="nav_menu">Categorie</a>
+        <a class="nav_menu">Stijl</a>
+        <div class="nav_right">
+            <a href="aanmelden.php" class="nav_menu">Aanmelden</a>
+            <a href="inloggen.php" class="nav_menu">Inloggen</a>
+            <img src="Images/winkelwagen.jpg" width="50px">
+        </div>
+    </div>
