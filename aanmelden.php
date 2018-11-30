3fdc4983e38ab20eb68c06d889f1106464438076
KaHongTam <kahongtam@hotmail.com>
2018-11-30 10:34:01 +0100

    footer.php aangemaakt en verwerkt op alle paginas

--- aanmelden.php
+++ aanmelden.php
@@ -55,12 +55,7 @@
             </div>
         </div>
     </div>
-    <div class="footer">
-            <a href="https://www.twitter.com/kahongtam1" target="_blank"><img src="Images\social_twitter.png"></a>
-            <a href="https://www.facebook.com/kahongtam" target="_blank"><img src="Images\social_facebook3.png"></a>
-            <a href="mailto:kahongtam@hotmail.com" target="_blank"><img src="Images\social_email.png"></a>
-        </div>
-        <div class="end"></div>
+    <?php include "footer.php" ?>
     <script src="aanmelden.js"></script>
 </body>
 </html>
--- shop.php
+++ shop.php
@@ -41,13 +41,7 @@
             </div>
         </div>
     </div>
-        <div class="footer">
-            <a href="https://www.twitter.com/kahongtam1" target="_blank"><img src="Images\social_twitter.png"></a>
-            <a href="https://www.facebook.com/kahongtam" target="_blank"><img src="Images\social_facebook3.png"></a>
-            <a href="mailto:kahongtam@hotmail.com" target="_blank"><img src="Images\social_email.png"></a>
-        </div>
-        <div class="end"></div>
-
+    <?php include "footer.php" ?>
     <script src="shop.js"></script>
 </body>
 </html>
